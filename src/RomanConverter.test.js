3dab6ce151f3cf77d12aa7736327bc9edbbb0e55
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-15 13:09:20 +0200

    05_failed RomanConverter should not convert 0 to any roman number

--- src/RomanConverter.test.js
+++ src/RomanConverter.test.js
@@ -31,4 +31,11 @@ describe('<RomanConverter />', () => {
             getByText("Roman: V")
         }).not.toThrow();
     });
+    it('should not convert 0 to any roman number', function () {
+        const {getByLabelText, getByText} = render(<RomanConverter />)
+        fireEvent.change(getByLabelText(/arabic/i), {target: {value: "0"}});
+        expect(() => {
+            getByText("Roman: none")
+        }).not.toThrow();
+    });
 })
