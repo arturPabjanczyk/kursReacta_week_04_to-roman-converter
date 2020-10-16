7102b335d26fa081597e34168e50bf84b0ff27ba
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-16 12:06:13 +0200

    17_passed

--- src/lib/converters.js
+++ src/lib/converters.js
@@ -1,44 +1,22 @@
 function toRoman(arabic) {
-    let roman = "none";
-
-    if (arabic == 1) {
-        roman = "I"
+    const romanSymbols = [
+        [1, "I"], [4, "IV"], [5, "V"], [9, "IX"], [10, "X"], [40, "XL"], [50, "L"],
+        [90, "XC"], [100, "C"], [400, "CD"], [500, "D"], [900, "CM"], [1000, "M"],
+    ]
+    const romanSymbolsSorted = [...romanSymbols].sort((a, b) => b[0] - a[0])
+    let roman = "";
+    let i = 0;
+    if (arabic === 0) {
+        return "none";
     }
-    if (arabic == 4) {
-        roman = "IV"
-    }
-    if (arabic == 5) {
-        roman = "V"
-    }
-    if (arabic == 9) {
-        roman = "IX"
-    }
-    if (arabic == 10) {
-        roman = "X"
-    }
-    if (arabic == 40) {
-        roman = "XL"
-    }
-    if (arabic == 50) {
-        roman = "L"
-    }
-    if (arabic == 90) {
-        roman = "XC"
-    }
-    if (arabic == 100) {
-        roman = "C"
-    }
-    if (arabic == 400) {
-        roman = "CD"
-    }
-    if (arabic == 500) {
-        roman = "D"
-    }
-    if (arabic == 900) {
-        roman = "CM"
-    }
-    if (arabic == 1000) {
-        roman = "M"
+    while (arabic > 0 && i < romanSymbolsSorted.length) {
+        const currentRomanSymbol = romanSymbolsSorted[i];
+        if (currentRomanSymbol[0] <= arabic) {
+            roman += currentRomanSymbol[1];
+            arabic -= currentRomanSymbol[0];
+        } else {
+            i++
+        }
     }
     return roman;
 }
