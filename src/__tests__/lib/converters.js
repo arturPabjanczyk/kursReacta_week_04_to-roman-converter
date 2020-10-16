084cc254bbdf839a61e777bf37621c0986d9c277
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-16 12:09:37 +0200

    refactor

--- src/__tests__/lib/converters.js
+++ src/__tests__/lib/converters.js
@@ -1,29 +1,9 @@
 import {toRoman} from "../../lib/converters"
 
 describe('toRoman()', () => {
-    it('should not convert 0 to any roman number', function () {
-        expect(toRoman(0)).toEqual("none")
-    });
-    it('should convert 2019 to MMXIX', function () {
-        expect(toRoman(2019)).toEqual("MMXIX")
-    });
-});
-describe('toRoman()', () => {
     it.each([
-        [1, "I"],
-        [2, "II"],
-        [4, "IV"],
-        [5, "V"],
-        [9, "IX"],
-        [10, "X"],
-        [40, "XL"],
-        [50, "L"],
-        [90, "XC"],
-        [100, "C"],
-        [400, "CD"],
-        [500, "D"],
-        [900, "CM"],
-        [1000, "M"]
+        [0, "none"], [1, "I"],[2, "II"],[4, "IV"],[5, "V"],[9, "IX"],[10, "X"],[40, "XL"],[50, "L"],
+        [90, "XC"],[100, "C"],[400, "CD"],[500, "D"],[900, "CM"],[1000, "M"], [2019, "MMXIX"]
     ])('should  convert %d to %s', (arabic, expectedRoman) => {
         expect(toRoman(arabic)).toEqual(expectedRoman)
     });
