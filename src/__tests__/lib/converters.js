db11f15ab389c4c0246b1fdd62899c9bad29f34c
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-16 08:08:19 +0200

    08_failed RomanConverter should convert 10 to X

--- src/__tests__/lib/converters.js
+++ src/__tests__/lib/converters.js
@@ -13,7 +13,8 @@ describe('toRoman()', () => {
         [1, "I"],
         [4, "IV"],
         [5, "V"],
-        [9, "IX"]
+        [9, "IX"],
+        [10, "X"]
     ])('should  convert %d to %s', (arabic, expectedRoman) => {
         expect(toRoman(arabic)).toEqual(expectedRoman)
     });
