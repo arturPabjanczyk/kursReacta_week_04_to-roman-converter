6c348179017fa30f342d80c9c5ffd597f5cb1c4f
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-16 08:18:13 +0200

    13_failed RomanConverter should convert 400 to CD

--- src/__tests__/lib/converters.js
+++ src/__tests__/lib/converters.js
@@ -18,7 +18,8 @@ describe('toRoman()', () => {
         [40, "XL"],
         [50, "L"],
         [90, "XC"],
-        [100, "C"]
+        [100, "C"],
+        [400, "CD"]
     ])('should  convert %d to %s', (arabic, expectedRoman) => {
         expect(toRoman(arabic)).toEqual(expectedRoman)
     });
