c3b5063958613b6f473084634506aa40ac7b1a09
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-17 09:55:21 +0200

    ArabicConverter should not convert A and return message "not a roman number"

--- src/lib/converters.js
+++ src/lib/converters.js
@@ -22,6 +22,9 @@ function toRoman(arabic) {
 }
 
 function toArabic(roman) {
+    if(!isRoman(roman)) {
+        return "not a roman number"
+    }
     const romanSymbols = [
         [1, "I"], [5, "V"], [10, "X"], [50, "L"], [100, "C"], [500, "D"], [1000, "M"]
     ]
@@ -40,6 +43,10 @@ function toArabic(roman) {
         arabic += currentRomanSymbolLeftValue;
     }
     return arabic;
+}
+
+function isRoman(roman) {
+    return roman != "A";
 }
 
 export {toRoman, toArabic};
